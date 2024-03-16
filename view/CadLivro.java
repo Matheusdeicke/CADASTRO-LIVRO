85ebdebfb8ecbb4582323eb8b17079a775f0f63d
Matheus <deickematheus@gmail.com>
2024-03-16 16:29:50 -0300

    NEW VERSION

--- view/CadLivro.java
+++ view/CadLivro.java
@@ -2,14 +2,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 
-
 public class CadLivro extends JFrame {
 
     private JButton btnOk, btnCancelar;
     private JTextField txtId, txtTitulo, txtAutor, txtCat, txtData;
     private JLabel CompId, CompTitulo, CompAutor, CompCat, CompData;
     private JPanel p1 = new JPanel();
-    private JPanel p2 = new JPanel();
 
     public CadLivro() {
         super("Cadastro Livro");
@@ -17,9 +15,8 @@ public class CadLivro extends JFrame {
         Container c = getContentPane();
 
         setSize(400, 300); // Tamanho da Janela
-        c.setLayout(new GridLayout(2, 1)); // Define o layout do container básico
+        c.setLayout(new FlowLayout()); // Define o layout do container básico
         p1.setLayout(new GridLayout(6, 2)); // Define o layout dos Panels
-        p2.setLayout(new FlowLayout()); // Alterado para BorderLayout
 
         // COMPONENTES
         CompId = new JLabel("ID:");
@@ -40,10 +37,27 @@ public class CadLivro extends JFrame {
         btnOk = new JButton("OK");
         btnCancelar = new JButton("Cancelar");
 
-        btnOk.setMnemonic(KeyEvent.VK_O); // Letra O
-        btnCancelar.setMnemonic(KeyEvent.VK_C); // Letra C
 
-        // Adicione os componentes ao painel p1 e p2
+        // SETA TODOS OS JLABEL PARA A DIREITA
+        CompId.setHorizontalAlignment(SwingConstants.RIGHT);
+        CompTitulo.setHorizontalAlignment(SwingConstants.RIGHT);
+        CompAutor.setHorizontalAlignment(SwingConstants.RIGHT);
+        CompCat.setHorizontalAlignment(SwingConstants.RIGHT);
+        CompData.setHorizontalAlignment(SwingConstants.RIGHT);
+
+        // SETA UMA FONTE DIFERENTE
+        Font fontePersonalizada = new Font("Times New Roman", Font.BOLD, 14);
+
+        CompId.setFont(fontePersonalizada);
+        CompTitulo.setFont(fontePersonalizada);
+        CompAutor.setFont(fontePersonalizada);
+        CompCat.setFont(fontePersonalizada);
+        CompData.setFont(fontePersonalizada);
+
+        btnOk.setMnemonic(KeyEvent.VK_O); // Letra O - ALT + O
+        btnCancelar.setMnemonic(KeyEvent.VK_C); // Letra C - ALT + C
+
+        // Adiciona os componentes no P1
         p1.add(CompId);
         p1.add(txtId);
         p1.add(CompTitulo);
@@ -54,12 +68,10 @@ public class CadLivro extends JFrame {
         p1.add(txtCat);
         p1.add(CompData);
         p1.add(txtData);
-
-        p2.add(btnOk); // Posicione o botão OK à esquerda
-        p2.add(btnCancelar); // Posicione o botão Cancelar à direita
-
+        p1.add(btnOk);
+        p1.add(btnCancelar);
         c.add(p1);
-        c.add(p2);
+
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
